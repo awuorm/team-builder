7c769b5f4e76adfce003033d9ac9cfabe5887819
Mildred Awuor <otieno.awuor.m@gmail.com>
2019-09-18 15:31:51 +0300

    rendered team component

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -2,13 +2,28 @@ import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {Route} from "react-router-dom";
+import TeamsList from "../src/Components/TeamsList";
 
-const [teamList, setTeamList] = useState([]);
+ const initialTeam = [
+  {name: "Mildred",
+    teamname: "Kiboko",
+    score: "10"  
+},
+{name: "Amina",
+    teamname: "Simba",
+    score: "0"  
+},
+{name: "Barak",
+    teamname: "Farasi",
+    score: "4"  
+}
+]
 
 function App() {
+  const [teamList, setTeamList] = useState(initialTeam);
   return (
     <div className="App">
-      <Route render={() => <TeamsList></TeamsList>}></Route>
+      <Route render={(props) => <TeamsList {...props} initialTeam={initialTeam}></TeamsList>}></Route>
     </div>
   );
 }
---
+++ team-builder/src/Components/TeamsList.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const TeamsList = (props) => {
+    const {initialTeam} = props;
+    return (
+            initialTeam.map((member) => (
+                <>
+                <p>Name:{member.name}</p>
+                <p>Team Name:{member.teamname}</p>
+                <p>Score:{member.score}</p>
+                </>
+            ))
+            
+     
+    )
+};
+
+export default TeamsList;
